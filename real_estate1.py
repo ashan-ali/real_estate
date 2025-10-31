db8f24dd23511c91128a7ebc575855e316c720d2
ashan-ali <alimohdashan@gmail.com>
2025-10-31 12:43:56 +0530

    real_estate1.py

--- real_estate1.py
+++ real_estate1.py
@@ -50,11 +50,13 @@ elif men == 'Prediction':
     #aprice=st.slider("Select house price of unit area", min_value = 10.0, max_value=120.0 , value= 15.0)
 
     pr = model.predict([[age, dst, con]])
+    st.image("processing.gif")
     if st.button("Now Predict House price"):
         st.write(f"House price is ===> {pr[0]}")
     
     
     
+
 
 
 
